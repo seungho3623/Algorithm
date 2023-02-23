12abb6184e621889051e09ddc52a38f4cee8b3da
Seungho Song <90664405+seungho3623@users.noreply.github.com>
2023-02-23 10:25:41 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -4,12 +4,10 @@
 using namespace std;
 
 bool solution(string s) {
-    if(s.size() != 4 && s.size() != 6) return false;
-    
     for(char c : s)
     {
         if(c < 48 || c > 57)
             return false;
     }
-    return true;
+    return (s.size() != 4 && s.size() != 6) ? false : true;
 }
