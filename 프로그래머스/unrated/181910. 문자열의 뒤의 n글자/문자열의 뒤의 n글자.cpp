b0be1de66baedc1d897e5684fef6ddb4f3dc8483
Seungho Song <90664405+seungho3623@users.noreply.github.com>
2023-06-07 13:51:10 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    reverse(my_string.begin(), my_string.end());
+    
+    for(auto i = 0; i < n; i++)
+        answer += my_string[i];
+    
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
