7d3ac309506379fc16c038c5a05146f448f6b424
Seungho Song <90664405+seungho3623@users.noreply.github.com>
2023-10-20 17:09:38 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+int main(int argc, char const *argv[]) {   
+    auto a=0, b=0;
+    cin >> a >> b;
+    cout << a+b << endl;
+    cout << a-b << endl;
+    cout << a*b << endl;
+    cout << a/b << endl;
+    cout << a%b << endl;
+    return 0;
+}
