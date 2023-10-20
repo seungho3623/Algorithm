5e01c6dc3135d54cde71a56524867b877071f90f
Seungho Song <90664405+seungho3623@users.noreply.github.com>
2023-10-20 17:29:33 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+    int y;
+ 
+    cin >> y;
+ 
+    if (y % 4 == 0) {
+        if (y % 400 == 0 || y % 100 != 0) { 
+            cout << 1;
+        } 
+        else {
+            cout << 0;
+        }
+    } else {
+        cout << 0;
+    }
+ 
+    return 0;
+}
