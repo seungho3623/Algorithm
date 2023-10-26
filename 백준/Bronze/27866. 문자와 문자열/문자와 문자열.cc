f91ff62b3aedce70a87bcdbc20b6280f563c4149
Seungho Song <90664405+seungho3623@users.noreply.github.com>
2023-10-26 23:33:15 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int main(int argc, char const *argv[]) {   
+    string S;
+    int t;
+    
+    cin >> S >> t;
+    cout << S[t-1];
+        
+    return 0;
+}
