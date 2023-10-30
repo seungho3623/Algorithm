512f1511f369bd47ad37cf1aa7ccf61fd83c4cf8
Seungho Song <90664405+seungho3623@users.noreply.github.com>
2023-10-30 23:25:55 +0900

    Update 단어 공부.cc

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -2,29 +2,33 @@
 
 using namespace std;
 
-int alpha[26], cnt =0;
+int alpha[26];
 string input;
 //아스키코드 : 대문자 65~90, 소문자 97~122
 
 int main() {
     cin >> input;
 
-    for(int i=0;i<input.length();i++) {
-        if(input[i]<97) alpha[input[i] - 'A']++; 
+    for(int i = 0; i < input.length(); i++) {
+        if(input[i] < 'a') alpha[input[i] - 'A']++; 
         else alpha[input[i] - 'a']++; 
     }
 
     int max = 0, max_indx=0;
 
-    for(int i=0;i<26;i++) {
+    for(int i = 0; i < 26; i++) {
         if(max<alpha[i]) {
             max=alpha[i];
             max_indx = i;
         }
     }
-    for(int i=0;i<26;i++) {
-        if(max==alpha[i]) cnt++;
+    for(int i = 0; i < 26; i++) {
+        if(max_indx == i) continue;
+        if(max==alpha[i]){
+            cout << "?";
+            return 0;
+        }
     }
-    if(cnt>1) cout << "?";
-    else cout << (char)(max_indx + 'A');
+    cout << (char)(max_indx + 'A');
+    return 0;
 }
