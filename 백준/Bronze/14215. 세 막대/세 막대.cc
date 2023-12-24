4cf458fa4eaea01f281358284eddba6b1a45ecad
Seungho Song <90664405+seungho3623@users.noreply.github.com>
2023-12-24 19:16:01 +0900

    [Bronze III] Title: 세 막대, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int a[3];
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a, a + 3);
+    if(a[0] + a[1] > a[2]) cout << a[0] + a[1] + a[2];
+    else cout << (a[0] + a[1]) * 2 - 1;
+    return 0;
+}
